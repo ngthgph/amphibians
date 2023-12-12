5be15c8b0b92c1ec1a95432100851d9bd9480fc1
ngthgph <ngthgph112@gmail.com>
2023-12-12 23:59:51 +0700

    Create relationship between data and UI layers

--- app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.amphibians.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibians.AmphibiansApplication
+import com.example.amphibians.data.AmphibianInformationRepository
 import com.example.amphibians.network.AmphibianInformation
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface AmphibiansUiState {
     data class Success(val amphibians: List<AmphibianInformation>): AmphibiansUiState
@@ -9,10 +22,34 @@ sealed interface AmphibiansUiState {
     object Error: AmphibiansUiState
 }
 
-class AmphibiansViewModel: ViewModel() {
+class AmphibiansViewModel(
+    private val amphibiansInformationRepository: AmphibianInformationRepository
+): ViewModel() {
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as AmphibiansApplication)
+                val amphibiansInformationRepository = application.container.amphibianInformationRepository
+                AmphibiansViewModel(amphibiansInformationRepository = amphibiansInformationRepository)
+            }
+        }
+    }
     init {
         getAmphibians()
     }
-    fun getAmphibians() {
+    var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+        private set
+
+    private fun getAmphibians() {
+        viewModelScope.launch {
+            amphibiansUiState = try {
+                AmphibiansUiState.Success(amphibiansInformationRepository.getAmphibianInformation())
+            } catch (e: IOException) {
+                AmphibiansUiState.Error
+            } catch (e: HttpException) {
+                AmphibiansUiState.Error
+            }
+        }
     }
 }
